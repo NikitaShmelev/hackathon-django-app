6ed331ee052eb52baf3ad4b04d2d8785c6312fab
n.shemelyov <shmelev507@gmail.com>
2024-06-08 15:58:40 +0200

    field models

--- home/models/base_model.py
+++ home/models/base_model.py
@@ -3,8 +3,6 @@ from django.db import models
 
 class BaseModel(models.Model):
     id = models.BigAutoField(primary_key=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
