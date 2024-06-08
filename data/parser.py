e9fa989390ef6576656ff83fa1f229f25447436e
n.shemelyov <shmelev507@gmail.com>
2024-06-08 14:07:24 +0200

    started parsing

---
+++ data/parser.py
@@ -0,0 +1,28 @@
+from GTFS import *
+import os
+
+
+files: list[str] = ['transfers.txt', 'agency.txt', 'calendar_dates.txt', 'stop_times.txt', 'shapes.txt', 'trips.txt', 'feed_info.txt', 'stops.txt', 'calendar.txt', 'routes.txt']
+
+dir_name: str = "GTFS"
+
+
+parsed_data: dict[str, list[dict[str, str]]] = {
+    
+}
+
+for file_name in files:
+    file = open(f"{dir_name}/{file_name}")
+    parsed_data[file_name] = []
+    headers: None | list[str] = None
+    for line_num, line in enumerate(file.readlines()):
+        if line_num == 0:
+            headers = line.strip().split(",")
+        else:
+            line_data = line.strip().split(",")
+            breakpoint()
+            print()
+            
+    break
+
+
