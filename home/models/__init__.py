97bedaf7a99f4a857f1b8b85dab2808c716d7595
n.shemelyov <shmelev507@gmail.com>
2024-06-08 15:25:02 +0200

    models

--- home/models/__init__.py
+++ home/models/__init__.py
@@ -1,8 +1,18 @@
 from .base_model import BaseModel
 from .post import Post
 from .trip_file import TripFile
-
+from .models import (
+    Agency, Calendar, CalendarDates,
+    FeedInfo, Routes, Shapes,
+    StopTimes, Stops, Transfers,
+    Trips,
+    )
 __all__ = [
     "BaseModel",
     "TripFile",
+    "Post",
+    "Agency", "Calendar", "CalendarDates",
+    "FeedInfo", "Routes", "Shapes",
+    "StopTimes", "Stops", "Transfers",
+    "Trips",
 ]
