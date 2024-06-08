c626711684dfebbb4646bd86cc2407b0d88a3dc0
n.shemelyov <shmelev507@gmail.com>
2024-06-08 14:47:07 +0200

    commit

--- home/views.py
+++ home/views.py
@@ -1,10 +1,11 @@
-from home.models import Post
+from home.models import Post, TripFile
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from .forms import PostForm
 from django.views.generic import ListView
+from django.views.generic.edit import FormView
 
-
+from home.forms import TripFileForm
 class IndexView(ListView):
     queryset = Post.objects.all()
     template_name = "index.html"
@@ -17,3 +18,13 @@ class PostCreateView(CreateView):
     form_class = PostForm
     template_name = "create_view.html"
     success_url = reverse_lazy("home_page")
+
+
+class FileUploadView(FormView):
+    template_name = 'upload.html'
+    success_url = reverse_lazy("home_page")
+    form_class = TripFileForm
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
