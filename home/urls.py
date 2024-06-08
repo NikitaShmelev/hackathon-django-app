d2612584b0ad26f000bf88432e60c95e4ca46ed6
n.shemelyov <shmelev507@gmail.com>
2024-06-09 01:56:05 +0200

    coś zadziałało

--- home/urls.py
+++ home/urls.py
@@ -7,13 +7,12 @@ urlpatterns = [
     path("", IndexView.as_view(), name="home_page"),
     path("create/", PostCreateView.as_view(), name="create_post"),
     path("upload_file/", FileUploadView.as_view(), name="upload"),
+    path('upload/', FileUploadView.as_view(), name='file_upload'),
     
     path("transfers/map/", TransferMapView.as_view(), name="transfer_map"),
     path('api/transfers/', transfers_json, name='transfers_json'),
-    # path('api/stops_near_location/', stops_near_location, name='stops_near_location'),
-    # path("transfers/api/transfers/", transfers_json, name="transfers_json"),
-    path('upload/', FileUploadView.as_view(), name='file_upload'),
+
     path('api/stops_near_location/', stops_near_location, name='stops_near_location'),
+    path('api/transfers_json/', transfers_json, name='transfers_json'),
     path('api/transfers_for_stop/<int:stop_id>/', transfers_for_stop, name='transfers_for_stop'),
-    # path('api/routes_through_stop/<int:stop_id>/', routes_through_stop, name='routes_through_stop'),
 ]
--- home/views.py
+++ home/views.py
@@ -96,3 +96,91 @@ def transfers_json(request):
                 'min_transfer_time': transfer.min_transfer_time
             })
     return JsonResponse(transfer_data, safe=False)
+
+def transfers_json(request):
+    transfers = Transfers.objects.all()
+    transfer_data = []
+    for transfer in transfers:
+        from_stop = Stops.objects.filter(stop_id=transfer.from_stop_id).first()
+        to_stop = Stops.objects.filter(stop_id=transfer.to_stop_id).first()
+        if from_stop and to_stop:
+            transfer_data.append({
+                'from_stop': {
+                    'id': from_stop.stop_id,
+                    'lat': from_stop.stop_lat,
+                    'lon': from_stop.stop_lon,
+                    'name': from_stop.stop_name
+                },
+                'to_stop': {
+                    'id': to_stop.stop_id,
+                    'lat': to_stop.stop_lat,
+                    'lon': to_stop.stop_lon,
+                    'name': to_stop.stop_name
+                },
+                'transfer_type': transfer.transfer_type,
+                'min_transfer_time': transfer.min_transfer_time
+            })
+    return JsonResponse(transfer_data, safe=False)
+
+def transfers_for_stop(request, stop_id):
+    # Find all trip_ids for the given stop
+    trip_ids = StopTimes.objects.filter(stop_id=stop_id).values_list('trip_id', flat=True)
+    
+    if not trip_ids:
+        return JsonResponse([], safe=False)
+
+    # Find all transfers related to these trips
+    related_stop_ids = StopTimes.objects.filter(trip_id__in=trip_ids).values_list('stop_id', flat=True)
+    transfers = Transfers.objects.filter(
+        from_stop_id__in=related_stop_ids,
+        to_stop_id__in=related_stop_ids
+    )
+
+    transfer_data = []
+    for transfer in transfers:
+        from_stop = Stops.objects.filter(stop_id=transfer.from_stop_id).first()
+        to_stop = Stops.objects.filter(stop_id=transfer.to_stop_id).first()
+        if from_stop and to_stop:
+            transfer_data.append({
+                'from_stop': {
+                    'id': from_stop.stop_id,
+                    'lat': from_stop.stop_lat,
+                    'lon': from_stop.stop_lon,
+                    'name': from_stop.stop_name
+                },
+                'to_stop': {
+                    'id': to_stop.stop_id,
+                    'lat': to_stop.stop_lat,
+                    'lon': to_stop.stop_lon,
+                    'name': to_stop.stop_name
+                },
+                'transfer_type': transfer.transfer_type,
+                'min_transfer_time': transfer.min_transfer_time
+            })
+    return JsonResponse(transfer_data, safe=False)
+
+def stops_near_location(request):
+    # Get bounds from the request
+    north_east_lat = float(request.GET.get("north_east_lat"))
+    north_east_lon = float(request.GET.get("north_east_lon"))
+    south_west_lat = float(request.GET.get("south_west_lat"))
+    south_west_lon = float(request.GET.get("south_west_lon"))
+
+    # Filter stops within the bounds
+    stops_within_bounds = Stops.objects.filter(
+        stop_lat__lte=north_east_lat,
+        stop_lat__gte=south_west_lat,
+        stop_lon__lte=north_east_lon,
+        stop_lon__gte=south_west_lon
+    )
+
+    stops_data = []
+    for stop in stops_within_bounds:
+        stops_data.append({
+            'id': stop.stop_id,
+            'name': stop.stop_name,
+            'lat': stop.stop_lat,
+            'lon': stop.stop_lon
+        })
+
+    return JsonResponse(stops_data, safe=False)
