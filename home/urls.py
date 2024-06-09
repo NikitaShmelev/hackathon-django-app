872822fa3d2575bdc7a3e423aba38c546e4d83cc
VladimirKrutko <krutkovova24@gmail.com>
2024-06-09 02:23:26 +0200

    Refactor URL patterns and views for GTFS data handling
    This commit updates the URL patterns and views in the `urls.py` and `views.py` files to handle GTFS data. New URL patterns are added for retrieving stops, shapes, and routes based on route and stop IDs. The corresponding view functions `stops_for_route()`, `shape_for_route()`, and `routes_for_stop()` are implemented to fetch the required data from the database and return JSON responses. These changes enhance the functionality and maintainability of the codebase.
    
    Note: This commit message follows the conventional format of "feat: <description>".

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import IndexView, PostCreateView, FileUploadView
+from .views import *
 
 
 urlpatterns = [
@@ -7,5 +7,7 @@ urlpatterns = [
     path("create/", PostCreateView.as_view(), name="create_post"),
     path('upload_file/', FileUploadView.as_view(), name='upload'),
     path('upload/', FileUploadView.as_view(), name='file_upload'),
-
+    path('stops/<str:route_id>/', stops_for_route, name='stops_for_route'),
+    path('shape/<str:route_id>/', shape_for_route, name='shape_for_route'),
+    path('routes/<str:stop_id>/', routes_for_stop, name='routes_for_stop'),
 ]
